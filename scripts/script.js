b57dafb97a324c1a7e97c3325db899e0f6333f83
beareditor <ursamajor@ursamajorsociety.com>
2019-03-22 12:35:32 -0400

    Added clues to goal

--- scripts/script.js
+++ scripts/script.js
@@ -22,17 +22,17 @@ function checkOne(){
 	var c = "Dubhe";
 	var t = document.getElementById("out1");
 	var p = document.getElementById("p1");
+	var x = document.getElementById("code1");
 	
-	var v = document.getElementById("pic1");
 	var cl = document.getElementById("clue1");
 	
 	if (p.value == c || h >= i){
 		t.innerHTML = "One was the Dean of Women and a Professor of Spanish.";
+		x.innerHTML = "UF FRIRA FVK FRIRA";
 		if (h < i){
 			document.cookie = "highestsolved="+i+";";
 		}
 		cl.style.display = "block";
-		v.style.display = "block";
 	}
 }
 function checkTwo(){
@@ -40,15 +40,19 @@ function checkTwo(){
 	var c = "Loth";
 	var t = document.getElementById("out2");
 	var p = document.getElementById("p2");
+	var x = document.getElementById("code2");
 	
 	var cl = document.getElementById("clue2");
+	var v = document.getElementById("pic2");
 	
 	if (p.value == c || h >= i){
 		t.innerHTML = "One was a Professor of Physics, heading the department.";
+		x.innerHTML = "J JAX ZFMR";
 		if (h < i){
 			document.cookie = "highestsolved="+i+";";
 		}
 		cl.style.display = "block";
+		v.style.display = "block";
 	}
 	
 }
@@ -57,11 +61,13 @@ function checkThree(){
 	var c = "Copal";
 	var t = document.getElementById("out3");
 	var p = document.getElementById("p3");
+	var x = document.getElementById("code3");
 	
 	var cl = document.getElementById("clue3");
 	
 	if (p.value == c || h >= i){
 		t.innerHTML = "One was an educator, known for working to open colleges to all.";
+		x.innerHTML = "ABBBAABBABAABAA ABBABABAAAABBABAABAA BBAABAABAABAAABABBBA BAABAABAAA";
 		if (h < i){
 			document.cookie = "highestsolved="+i+";";
 		}
@@ -73,11 +79,13 @@ function checkFour(){
 	var c = "Bemiss";
 	var t = document.getElementById("out4");
 	var p = document.getElementById("p4");
+	var x = document.getElementById("code4");
 	
 	var cl = document.getElementById("clue4");
 	
 	if (p.value == c || h >= i){
 		t.innerHTML = "And the last was from far away and an accomplished linguist.";
+		x.innerHTML = "";
 		if (h < i){
 			document.cookie = "highestsolved="+i+";";
 		}
